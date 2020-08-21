ef3c79caec3ee45a020cade366d8310fca616048
Nahia Akter <nhaktr@gmail.com>
2020-08-21 00:46:29 -0400

    task one completed

--- generator.py
+++ generator.py
@@ -9,6 +9,26 @@ and vocabulary. Words are chosen at random.
 
 import random
 
+def getWords(filename):
+    files = open(filename)
+    temp_list = list()
+    for each_line in files:
+        each_line = each_line.strip()
+        temp_list.append(each_line)
+
+    words = tuple(temp_list)
+    files.close()
+
+    return words
+
+"""Getting words from the text files."""
+
+articles = getWords('articles.txt')
+nouns = getWords('nouns.txt')
+verbs = getWords('verbs.txt')
+prepositions = getWords('prepositions.txt')
+conjunctions = getWords('conjunctions.txt')
+adjectives = getWords('adjectives.txt')
 
 def sentence():
     """Builds and returns a sentence."""
